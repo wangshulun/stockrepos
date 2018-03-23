37e6961a5768fa2a8699981e58e3aa247832c5bd
wangshulun <wangshulun@fang.com>
2018-03-23 10:45:07 +0800

    添加 # type: 解决动态语言pycharm提示不全的问题

--- test.py
+++ test.py
@@ -4,12 +4,15 @@
 import tushare as ts
 import time as tm
 from sqlalchemy import create_engine
+import pandas as pd
 
 tmstr = tm.strftime("%Y-%m-%d", tm.localtime())
 engine = create_engine('mysql+pymysql://allen:qin88min@192.161.180.105/stock?charset=utf8')
 
 # 个股首个上市日期
-df = ts.get_stock_basics()
+df = ts.get_stock_basics() # type: pd.DataFrame
+
+
 print(df)
 
 # 存数据
